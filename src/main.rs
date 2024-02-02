253fc1d96fe90c92949b8326a358e5d896ea2f2a
Jorbon <ben.a.phillips@outlook.com>
2024-02-02 00:38:06 -0600

    some collision detection

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate glium;
 
 mod matfns;
+
 use matfns::{Mat4, Vec2, Vec3};
 
 use glium::{draw_parameters::DepthTest, framebuffer::{SimpleFrameBuffer, MultiOutputFrameBuffer}, glutin::{event::{Event, WindowEvent, ElementState, VirtualKeyCode}, event_loop::{ControlFlow, EventLoop}, dpi::{PhysicalPosition, PhysicalSize, LogicalSize}, window::{CursorGrabMode, WindowBuilder}, ContextBuilder}, index::PrimitiveType, texture::{depth_texture2d::DepthTexture2d, RawImage2d, SrgbTexture2d, Texture2d}, uniforms::{MagnifySamplerFilter, MinifySamplerFilter, Sampler, SamplerBehavior, SamplerWrapFunction}, vertex::Attribute, BackfaceCullingMode, Depth, Display, DrawParameters, IndexBuffer, Program, Surface, Vertex, VertexBuffer, VertexFormat};
@@ -41,7 +42,11 @@ impl Vertex for Vec3 {
 struct Object {
 	vertex_buffer: VertexBuffer<Vec3>,
 	index_buffer: IndexBuffer<u16>,
+	indices: Box<[(u16, u16, u16)]>,
+	edges: Box<[(u16, u16)]>,
 	transform: Mat4,
+	velocity: Vec3,
+	angular_velocity: Vec3,
 }
 
 impl Object {
@@ -51,10 +56,31 @@ impl Object {
 			core::slice::from_raw_parts(indices.as_ptr() as *const u16, indices.len() * 3)
 		}).unwrap();
 		
+		let mut edges = std::collections::HashSet::new();
+		for t in indices {
+			let (a, b, c) = match (t.0 < t.1, t.0 < t.2, t.1 < t.2) {
+				(true, true, true) => (t.0, t.1, t.2),
+				(true, true, false) => (t.0, t.2, t.1),
+				(true, false, true) => unreachable!(),
+				(true, false, false) => (t.2, t.0, t.1),
+				(false, true, true) => (t.1, t.0, t.2),
+				(false, true, false) => unreachable!(),
+				(false, false, true) => (t.1, t.2, t.0),
+				(false, false, false) => (t.2, t.1, t.0)
+			};
+			edges.insert((a, b));
+			edges.insert((a, c));
+			edges.insert((b, c));
+		}
+		
 		Self {
 			vertex_buffer,
 			index_buffer,
-			transform: Mat4::identity()
+			indices: indices.to_vec().into_boxed_slice(),
+			edges: edges.into_iter().collect::<Vec<(u16, u16)>>().into_boxed_slice(),
+			transform: Mat4::identity(),
+			velocity: Vec3(0.0, 0.0, 0.0),
+			angular_velocity: Vec3(0.0, 0.0, 0.0)
 		}
 	}
 }
@@ -163,7 +189,7 @@ fn main() {
 		]
 	);
 	
-	o1.transform = o1.transform.translate(0.0, 5.0, 0.0);
+	o1.transform = o1.transform.rotate_x(0.5).rotate_z(0.5).translate(0.0, 10.0, 0.0);
 	
 	
 	let floor = Object::new(&display, &[
@@ -175,6 +201,9 @@ fn main() {
 		(0, 2, 3),
 		(0, 3, 1)
 	]);
+	
+	//o1.angular_velocity = Vec3(0.2, 0.3, 0.5);
+	//floor.angular_velocity = Vec3(-0.05, 0.1, -0.02);
 	
 	
 	let mut objects = vec![o1, floor];
@@ -194,22 +223,26 @@ fn main() {
 	let mut space = false;
 	let mut shift = false;
 	
+	let mut run = false;
 	let mut capture = false;
 	let mut previous_mouse_pos = PhysicalPosition::<f64>::new(0.0, 0.0);
 	
 	
-	let mut a = 0.0f32;
+	let mut a = 0.3f32;
 	let mut b = 0.0f32;
 	let lspeed = 0.00025;
 	
-	let mut x = 0.0f32;
-	let mut y = 5.0f32;
-	let mut z = -4.0f32;
+	let mut x = 4.0f32;
+	let mut y = 6.0f32;
+	let mut z = -11.0f32;
 	let speed = 2.0;
+	
+	let g = 9.8;
+	
 	
 	let mut dummy = 0.0f32;
 	
-	let mut do_post_process = false;
+	let do_post_process = true;
 	let mut show_shadowmap = false;
 	
 	
@@ -247,11 +280,17 @@ fn main() {
 							VirtualKeyCode::Space => space = state,
 							VirtualKeyCode::LShift => shift = state,
 							
-							VirtualKeyCode::P => if state { do_post_process = !do_post_process; }
+							VirtualKeyCode::P => if state { run = !run; }
 							VirtualKeyCode::M => if state { show_shadowmap = !show_shadowmap; }
 							VirtualKeyCode::Comma => if state { dummy -= 0.1; }
 							VirtualKeyCode::Period => if state { dummy += 0.1; }
 							VirtualKeyCode::Slash => if state { dummy = 0.0; }
+							
+							VirtualKeyCode::R => if state {
+								objects[0].transform = objects[0].transform.set_position(Vec3(0.0, 10.0, 0.0));
+								objects[0].velocity = Vec3(0.0, 0.0, 0.0);
+								objects[0].angular_velocity = Vec3(0.0, 0.1, 0.2);
+							}
 							
 							VirtualKeyCode::Escape => if state && capture {
 								capture = false;
@@ -300,9 +339,12 @@ fn main() {
 				display.gl_window().window().request_redraw();
 			}
 			Event::RedrawRequested(_) => {
-				let now = std::time::SystemTime::now();
-				let dt = now.duration_since(previous_frame_time).unwrap().as_micros() as f32 * 1.0e-6;
-				previous_frame_time = now;
+				let dt = {
+					let now = std::time::SystemTime::now();
+					let dt = now.duration_since(previous_frame_time).unwrap().as_secs_f32();
+					previous_frame_time = now;
+					dt
+				};
 				
 				if dt < 1.0 {
 					avg_frame_time += dt * (dt - avg_frame_time);
@@ -338,14 +380,187 @@ fn main() {
 				
 				
 				
-				objects[0].transform.set_position(Vec3(0.0, dummy, 0.0));
+				
+				if run {
+					
+					objects[1].velocity = Vec3(0.0, 0.0, 0.0);
+					
+					
+					let new_transforms = (0..objects.len()).map(|i| {
+						let new_position = objects[i].transform.get_position() + objects[i].velocity * dt;
+						let mut new_transform = objects[i].transform;
+						if objects[i].angular_velocity.length_squared() > 0.0 {
+							new_transform = new_transform.rotate_axis(objects[i].angular_velocity.normalize(), objects[i].angular_velocity.length() * dt);
+						}
+						
+						objects[i].velocity += Vec3(0.0, -g * dt, 0.0);
+						
+						new_transform.set_position(new_position)
+					}).collect::<Vec<Mat4>>();
+					
+					
+					let transformed_vertices = (0..objects.len()).map(|i| objects[i].vertex_buffer.read().unwrap().iter().map(|v| (v.apply_transform(&objects[i].transform), v.apply_transform(&new_transforms[i]))).collect::<Vec<(Vec3, Vec3)>>()).collect::<Vec<Vec<(Vec3, Vec3)>>>();
+					
+					for i in 0..objects.len() { for j in 0..objects.len() {
+						if i == j { continue; }
+						for k in 0..transformed_vertices[i].len() {
+							let (this_v, next_v) = transformed_vertices[i][k];
+							for l in 0..objects[j].indices.len() {
+								let (a_index, b_index, c_index) = objects[j].indices[l];
+								let (this_a, next_a) = transformed_vertices[j][a_index as usize];
+								let (this_b, next_b) = transformed_vertices[j][b_index as usize];
+								let (this_c, next_c) = transformed_vertices[j][c_index as usize];
+								
+								
+								// 1) find t
+								let p0 = this_v - this_a;
+								let g0 = this_b - this_a;
+								let h0 = this_c - this_a;
+								let dp = next_v - next_a - p0;
+								let dg = next_b - next_a - g0;
+								let dh = next_c - next_a - h0;
+								
+								/*
+								p = p0 + dp * t
+								g = g0 + dg * t
+								h = h0 + dh * t
+								
+								g.cross(h).dot(p) == 0.0
+								
+								(g0 + dg * t).cross(h0 + dh * t).dot(p0 + dp * t) == 0.0
+								
+								(g0.cross(h0) + (dg.cross(h0) + g0.cross(dh))*t + dg.cross(dh)*t*t).dot(p0 + dp*t) == 0.0
+								
+								(
+									g0.cross(h0).dot(p0) +
+									((dg.cross(h0) + g0.cross(dh)).dot(p0) + g0.cross(h0).dot(dp)) *t +
+									(dg.cross(dh).dot(p0) + (dg.cross(h0) + g0.cross(dh)).dot(dp)) *t*t +
+									dg.cross(dh).dot(dp) *t*t*t
+								) == 0.0
+								*/
+								
+								let cubic_a = dg.cross(dh).dot(dp);
+								let cubic_b = dg.cross(dh).dot(p0) + (dg.cross(h0) + g0.cross(dh)).dot(dp);
+								let cubic_c = (dg.cross(h0) + g0.cross(dh)).dot(p0) + g0.cross(h0).dot(dp);
+								let cubic_d = g0.cross(h0).dot(p0);
+								
+								let t = if cubic_a.abs() > 1e-7 {
+									let b = cubic_b / cubic_a;
+									let c = cubic_c / cubic_a;
+									let d = cubic_d / cubic_a;
+									
+									let q = (3.0*c - b*b) / 9.0;
+									let r = b * (9.0*c - 2.0*b*b) / 54.0 - 0.5 * d;
+									
+									let discriminant = q*q*q + r*r;
+									if discriminant >= 0.0 {
+										let sqrtd = f32::sqrt(discriminant);
+										let t = -b / 3.0 + (r + sqrtd).cbrt() + (r - sqrtd).cbrt();
+										match t >= 0.0 && t <= 1.0 {
+											true => Some(t),
+											false => None
+										}
+									} else {
+										let dum1 = f32::acos(r / f32::sqrt(-q*q*q));
+										let r13 = 2.0 * f32::sqrt(-q);
+										let t1 = -b / 3.0 + r13 * f32::cos(dum1 / 3.0);
+										let t2 = -b / 3.0 + r13 * f32::cos((dum1 + 2.0*std::f32::consts::PI) / 3.0);
+										let t3 = -b / 3.0 + r13 * f32::cos((dum1 + 4.0*std::f32::consts::PI) / 3.0);
+										
+										match (t1 >= 0.0 && t1 <= 1.0, t2 >= 0.0 && t2 <= 1.0, t3 >= 0.0 && t3 <= 1.0, t1 < t2, t1 < t3, t2 < t3) {
+											(true, true, true, true, true, _) => Some(t1),
+											(true, true, true, false, _, true) => Some(t2),
+											(true, true, true, _, false, false) => Some(t3),
+											(true, true, false, true, _, _) => Some(t1),
+											(true, true, false, false, _, _) => Some(t2),
+											(true, false, true, _, true, _) => Some(t1),
+											(true, false, true, _, false, _) => Some(t3),
+											(false, true, true, _, _, true) => Some(t2),
+											(false, true, true, _, _, false) => Some(t3),
+											(true, false, false, _, _, _) => Some(t1),
+											(false, true, false, _, _, _) => Some(t1),
+											(false, false, true, _, _, _) => Some(t1),
+											(false, false, false, _, _, _) => None,
+											(true, true, true, false, true, false) | (true, true, true, true, false, true) => unreachable!()
+										}
+									}
+								} else if cubic_b.abs() > 1e-7 {
+									let b = cubic_c / cubic_b;
+									let c = cubic_d / cubic_b;
+									
+									let discriminant = b*b - 4.0*c;
+									if discriminant >= 0.0 {
+										let sqrtd = discriminant.sqrt();
+										let t1 = (-b + sqrtd) * 0.5;
+										let t2 = (-b - sqrtd) * 0.5;
+										
+										match (t1 >= 0.0 && t1 <= 1.0, t2 >= 0.0 && t2 <= 1.0, t1 < t2) {
+											(true, true, true) => Some(t1),
+											(true, true, false) => Some(t2),
+											(true, false, _) => Some(t1),
+											(false, true, _) => Some(t2),
+											(false, false, _) => None
+										}
+									} else {
+										None
+									}
+								} else {
+									let t = -cubic_d / cubic_c;
+									match t >= 0.0 && t <= 1.0 {
+										true => Some(t),
+										false => None
+									}
+								};
+								
+								
+								if let Some(t) = t {
+									let p = p0 + dp * t;
+									let g = g0 + dg * t;
+									let h = h0 + dh * t;
+									
+									let (larger, smaller) = match f32::max(g.0*g.1, f32::max(g.0*g.2, g.1*g.2)) > f32::max(h.0*h.1, f32::max(h.0*h.2, h.1*h.2)) {
+										true => (g, h),
+										false => (h, g)
+									};
+									
+									let (la, lb, sa, sb, pa, pb) = match (larger.0 < larger.1, larger.0 < larger.2, larger.1 < larger.2) {
+										(true, true, true) => (larger.2, larger.1, smaller.2, smaller.1, p.2, p.1),
+										(true, true, false) => (larger.1, larger.2, smaller.1, smaller.2, p.1, p.2),
+										(true, false, true) => unreachable!(),
+										(true, false, false) => (larger.1, larger.0, smaller.1, smaller.0, p.1, p.0),
+										(false, true, true) => (larger.2, larger.0, smaller.2, smaller.0, p.2, p.0),
+										(false, true, false) => unreachable!(),
+										(false, false, true) => (larger.0, larger.2, smaller.0, smaller.2, p.0, p.2),
+										(false, false, false) => (larger.0, larger.1, smaller.0, smaller.1, p.0, p.1)
+									};
+									
+									let s = (pa / la - pb / lb) / (sa / la - sb / lb);
+									let r = (pa - sa * s) / la;
+									// larger * r + smaller * s = p
+									
+									if r >= 0.0 && r <= 1.0 && s >= 0.0 && s <= 1.0 && r + s <= 1.0 {
+										run = false;
+									}
+								}
+							}
+						}
+					}}
+					
+					for i in 0..objects.len() {
+						objects[i].transform = new_transforms[i];
+					}
+				}
+				
+				
+				
+				
 				
 				
 				
 				let light_direction = Vec3(f32::cos(dummy), 2.0, f32::sin(dummy)).normalize();
 				
 				shadowmap.transform = Mat4::identity()
-					.translate(0.0, -5.0, 0.0)
+					.translate(0.0, 0.0, 0.0)
 					.rotate_y(f32::atan2(light_direction.0, -light_direction.2))
 					.rotate_x(f32::asin(-light_direction.1))
 					.translate(0.0, 0.0, -(shadowmap.far_distance + shadowmap.near_distance))
--- src/matfns.rs
+++ src/matfns.rs
@@ -1,34 +1,36 @@
-#[derive(Copy, Clone, Debug, Default)]
+
+
+#[derive(Copy, Clone, Debug, Default, PartialEq)]
 pub struct Vec2(pub f32, pub f32);
-#[derive(Copy, Clone, Debug, Default)]
+#[derive(Copy, Clone, Debug, Default, PartialEq)]
 pub struct Vec3(pub f32, pub f32, pub f32);
-#[derive(Copy, Clone, Debug)]
-pub struct Mat4(pub [[f32; 4]; 4]);
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Mat4(pub [[f32; 4]; 4]); // [column][row], [x][y], inner list is a column
 
 #[allow(dead_code)]
 impl Mat4 {
-	pub fn mult_mat4(&self, m: [[f32; 4]; 4]) -> Mat4 {
+	pub fn mult_mat4(&self, m: &Mat4) -> Mat4 {
 		Mat4([
 			[
-				self.0[0][0]*m[0][0] + self.0[1][0]*m[0][1] + self.0[2][0]*m[0][2] + self.0[3][0]*m[0][3],
-				self.0[0][1]*m[0][0] + self.0[1][1]*m[0][1] + self.0[2][1]*m[0][2] + self.0[3][1]*m[0][3],
-				self.0[0][2]*m[0][0] + self.0[1][2]*m[0][1] + self.0[2][2]*m[0][2] + self.0[3][2]*m[0][3],
-				self.0[0][3]*m[0][0] + self.0[1][3]*m[0][1] + self.0[2][3]*m[0][2] + self.0[3][3]*m[0][3],
+				self.0[0][0]*m.0[0][0] + self.0[1][0]*m.0[0][1] + self.0[2][0]*m.0[0][2] + self.0[3][0]*m.0[0][3],
+				self.0[0][1]*m.0[0][0] + self.0[1][1]*m.0[0][1] + self.0[2][1]*m.0[0][2] + self.0[3][1]*m.0[0][3],
+				self.0[0][2]*m.0[0][0] + self.0[1][2]*m.0[0][1] + self.0[2][2]*m.0[0][2] + self.0[3][2]*m.0[0][3],
+				self.0[0][3]*m.0[0][0] + self.0[1][3]*m.0[0][1] + self.0[2][3]*m.0[0][2] + self.0[3][3]*m.0[0][3],
 			], [
-				self.0[0][0]*m[1][0] + self.0[1][0]*m[1][1] + self.0[2][0]*m[1][2] + self.0[3][0]*m[1][3],
-				self.0[0][1]*m[1][0] + self.0[1][1]*m[1][1] + self.0[2][1]*m[1][2] + self.0[3][1]*m[1][3],
-				self.0[0][2]*m[1][0] + self.0[1][2]*m[1][1] + self.0[2][2]*m[1][2] + self.0[3][2]*m[1][3],
-				self.0[0][3]*m[1][0] + self.0[1][3]*m[1][1] + self.0[2][3]*m[1][2] + self.0[3][3]*m[1][3],
+				self.0[0][0]*m.0[1][0] + self.0[1][0]*m.0[1][1] + self.0[2][0]*m.0[1][2] + self.0[3][0]*m.0[1][3],
+				self.0[0][1]*m.0[1][0] + self.0[1][1]*m.0[1][1] + self.0[2][1]*m.0[1][2] + self.0[3][1]*m.0[1][3],
+				self.0[0][2]*m.0[1][0] + self.0[1][2]*m.0[1][1] + self.0[2][2]*m.0[1][2] + self.0[3][2]*m.0[1][3],
+				self.0[0][3]*m.0[1][0] + self.0[1][3]*m.0[1][1] + self.0[2][3]*m.0[1][2] + self.0[3][3]*m.0[1][3],
 			], [
-				self.0[0][0]*m[2][0] + self.0[1][0]*m[2][1] + self.0[2][0]*m[2][2] + self.0[3][0]*m[2][3],
-				self.0[0][1]*m[2][0] + self.0[1][1]*m[2][1] + self.0[2][1]*m[2][2] + self.0[3][1]*m[2][3],
-				self.0[0][2]*m[2][0] + self.0[1][2]*m[2][1] + self.0[2][2]*m[2][2] + self.0[3][2]*m[2][3],
-				self.0[0][3]*m[2][0] + self.0[1][3]*m[2][1] + self.0[2][3]*m[2][2] + self.0[3][3]*m[2][3],
+				self.0[0][0]*m.0[2][0] + self.0[1][0]*m.0[2][1] + self.0[2][0]*m.0[2][2] + self.0[3][0]*m.0[2][3],
+				self.0[0][1]*m.0[2][0] + self.0[1][1]*m.0[2][1] + self.0[2][1]*m.0[2][2] + self.0[3][1]*m.0[2][3],
+				self.0[0][2]*m.0[2][0] + self.0[1][2]*m.0[2][1] + self.0[2][2]*m.0[2][2] + self.0[3][2]*m.0[2][3],
+				self.0[0][3]*m.0[2][0] + self.0[1][3]*m.0[2][1] + self.0[2][3]*m.0[2][2] + self.0[3][3]*m.0[2][3],
 			], [
-				self.0[0][0]*m[3][0] + self.0[1][0]*m[3][1] + self.0[2][0]*m[3][2] + self.0[3][0]*m[3][3],
-				self.0[0][1]*m[3][0] + self.0[1][1]*m[3][1] + self.0[2][1]*m[3][2] + self.0[3][1]*m[3][3],
-				self.0[0][2]*m[3][0] + self.0[1][2]*m[3][1] + self.0[2][2]*m[3][2] + self.0[3][2]*m[3][3],
-				self.0[0][3]*m[3][0] + self.0[1][3]*m[3][1] + self.0[2][3]*m[3][2] + self.0[3][3]*m[3][3],
+				self.0[0][0]*m.0[3][0] + self.0[1][0]*m.0[3][1] + self.0[2][0]*m.0[3][2] + self.0[3][0]*m.0[3][3],
+				self.0[0][1]*m.0[3][0] + self.0[1][1]*m.0[3][1] + self.0[2][1]*m.0[3][2] + self.0[3][1]*m.0[3][3],
+				self.0[0][2]*m.0[3][0] + self.0[1][2]*m.0[3][1] + self.0[2][2]*m.0[3][2] + self.0[3][2]*m.0[3][3],
+				self.0[0][3]*m.0[3][0] + self.0[1][3]*m.0[3][1] + self.0[2][3]*m.0[3][2] + self.0[3][3]*m.0[3][3],
 			]
 		])
 	}
@@ -75,14 +77,26 @@ impl Mat4 {
 		])
 	}
 	
+	pub fn rotate_axis(&self, axis: Vec3, angle: f32) -> Mat4 {
+		let c = angle.cos();
+		let s = angle.sin();
+		Mat4([
+			[	c + axis.0*axis.0 * (1.0 - c),			axis.1*axis.0 * (1.0 - c) + axis.2 * s,	axis.2*axis.0 * (1.0 - c) - axis.1 * s,	0.0	],
+			[	axis.0*axis.1 * (1.0 - c) - axis.2 * s,	c + axis.1*axis.1 * (1.0 - c),			axis.2*axis.1 * (1.0 - c) + axis.0 * s,	0.0	],
+			[	axis.0*axis.2 * (1.0 - c) + axis.1 * s,	axis.1*axis.2 * (1.0 - c) - axis.0 * s,	c + axis.2*axis.2 * (1.0 - c),			0.0	],
+			[	0.0,									0.0,									0.0,									1.0	],
+		]).mult_mat4(self)
+	}
+	
 	pub fn get_position(&self) -> Vec3 {
 		Vec3(self.0[3][0], self.0[3][1], self.0[3][2])
 	}
 	
-	pub fn set_position(&mut self, v: Vec3) {
-		self.0[3][0] = v.0;
-		self.0[3][1] = v.1;
-		self.0[3][2] = v.2;
+	pub fn set_position(&self, v: Vec3) -> Mat4 {
+		Mat4([
+			self.0[0], self.0[1], self.0[2],
+			[v.0, v.1, v.2, self.0[3][3]]
+		])
 	}
 	
 	pub fn translate(&self, dx: f32, dy: f32, dz: f32) -> Mat4 {
@@ -110,28 +124,47 @@ impl Mat4 {
 
 
 #[allow(dead_code)]
-impl Vec3 {
-	pub fn length_squared(&self) -> f32 {
-		self.0*self.0 + self.1*self.1 + self.2*self.2
-	}
-	
-	pub fn normalize(&self) -> Vec3 {
-		let f = 1.0 / (self.0*self.0 + self.1*self.1 + self.2*self.2).sqrt();
-		Vec3(self.0*f, self.1*f, self.2*f)
-	}
-
-	#[inline]
-	pub fn normalize_fast(&self) -> Vec3 {
-		let length2: f32 = self.0*self.0 + self.1*self.1 + self.2*self.2;
-		let f: f32 = unsafe {
-			let i: u32 = *(&length2 as *const f32 as *const u32);
-			let i: u32 = 0x5f3759df - (i >> 1);
-			let y: f32 = *(&i as *const u32 as *const f32);
-			y * (1.5 - 0.5 * length2 * y * y)
-		};
-		Vec3(self.0*f, self.1*f, self.2*f)
-	}
+impl Vec2 {
+	#[inline] pub fn length_squared(self) -> f32 { self.0*self.0 + self.1*self.1 }
+	#[inline] pub fn length(self) -> f32 { self.length_squared().sqrt() }
+	#[inline] pub fn normalize(self) -> Self { let f = 1.0 / self.length(); Self(self.0*f, self.1*f) }
+	#[inline] pub fn dot(self, v: Self) -> f32 { self.0 * v.0 + self.1 * v.1 }
+	#[inline] pub fn cross(self, v: Self) -> f32 { self.0 * v.1 - self.1 * v.0 }
 }
+
+#[allow(dead_code)]
+impl Vec3 {
+	#[inline] pub fn length_squared(self) -> f32 { self.0*self.0 + self.1*self.1 + self.2*self.2 }
+	#[inline] pub fn length(self) -> f32 { self.length_squared().sqrt() }
+	#[inline] pub fn normalize(self) -> Self { let f = 1.0 / self.length(); Self(self.0*f, self.1*f, self.2*f) }
+	#[inline] pub fn dot(self, v: Self) -> f32 { self.0 * v.0 + self.1 * v.1 + self.2 * v.2 }
+	#[inline] pub fn cross(self, v: Self) -> Vec3 { Vec3(self.1 * v.2 - self.2 * v.1, self.2 * v.0 - self.0 * v.2, self.0 * v.1 - self.1 * v.0) }
+	#[inline] pub fn apply_transform(self, m: &Mat4) -> Self { Self(
+		m.0[0][0]*self.0 + m.0[1][0]*self.1 + m.0[2][0]*self.2 + m.0[3][0],
+		m.0[0][1]*self.0 + m.0[1][1]*self.1 + m.0[2][1]*self.2 + m.0[3][1],
+		m.0[0][2]*self.0 + m.0[1][2]*self.1 + m.0[2][2]*self.2 + m.0[3][2],
+	) }
+}
+
+impl std::ops::Add for Vec2 { type Output = Self; #[inline] fn add(self, rhs: Self) -> Self::Output { Self(self.0 + rhs.0, self.1 + rhs.1) } }
+impl std::ops::AddAssign for Vec2 { #[inline] fn add_assign(&mut self, rhs: Self) { *self = *self + rhs; } }
+impl std::ops::Sub for Vec2 { type Output = Self; #[inline] fn sub(self, rhs: Self) -> Self::Output { Self(self.0 - rhs.0, self.1 - rhs.1) } }
+impl std::ops::SubAssign for Vec2 { #[inline] fn sub_assign(&mut self, rhs: Self) { *self = *self - rhs; } }
+impl std::ops::Neg for Vec2 { type Output = Self; #[inline] fn neg(self) -> Self::Output { Self(-self.0, -self.1) } }
+impl std::ops::Mul<f32> for Vec2 { type Output = Self; #[inline] fn mul(self, rhs: f32) -> Self::Output { Self(self.0 * rhs, self.1 * rhs) } }
+impl std::ops::MulAssign<f32> for Vec2 { #[inline] fn mul_assign(&mut self, rhs: f32) { *self = *self * rhs; } }
+impl std::ops::Div<f32> for Vec2 { type Output = Self; #[inline] fn div(self, rhs: f32) -> Self::Output { self * (1.0/rhs) } }
+impl std::ops::DivAssign<f32> for Vec2 { #[inline] fn div_assign(&mut self, rhs: f32) { *self = *self / rhs; } }
+
+impl std::ops::Add for Vec3 { type Output = Self; #[inline] fn add(self, rhs: Self) -> Self::Output { Self(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2) } }
+impl std::ops::AddAssign for Vec3 { #[inline] fn add_assign(&mut self, rhs: Self) { *self = *self + rhs; } }
+impl std::ops::Sub for Vec3 { type Output = Self; #[inline] fn sub(self, rhs: Self) -> Self::Output { Self(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2) } }
+impl std::ops::SubAssign for Vec3 { #[inline] fn sub_assign(&mut self, rhs: Self) { *self = *self - rhs; } }
+impl std::ops::Neg for Vec3 { type Output = Self; #[inline] fn neg(self) -> Self::Output { Self(-self.0, -self.1, -self.2) } }
+impl std::ops::Mul<f32> for Vec3 { type Output = Self; #[inline] fn mul(self, rhs: f32) -> Self::Output { Self(self.0 * rhs, self.1 * rhs, self.2 * rhs) } }
+impl std::ops::MulAssign<f32> for Vec3 { #[inline] fn mul_assign(&mut self, rhs: f32) { *self = *self * rhs; } }
+impl std::ops::Div<f32> for Vec3 { type Output = Self; #[inline] fn div(self, rhs: f32) -> Self::Output { self * (1.0/rhs) } }
+impl std::ops::DivAssign<f32> for Vec3 { #[inline] fn div_assign(&mut self, rhs: f32) { *self = *self / rhs; } }
 
 
 
